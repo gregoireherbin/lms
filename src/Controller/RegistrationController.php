14c9ee4c5036ceb8466d12a708a486895845bf53
Greg pc-montmorillonm <gregoire.herbin@free.fr>
2026-03-22 16:35:29 +0100

    Ajout inscription

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistrationController extends AbstractController
+{
+    #[Route('/inscription', name: 'app_register')]
+    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $passwordHasher->hashPassword($user, $form->get('plainPassword')->getData())
+            );
+
+            // ⚡ rôle forcé à étudiant pour inscription publique
+            $user->setRoles(['ROLE_ETUDIANT']);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Inscription réussie !');
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -15,29 +15,30 @@ class UserType extends AbstractType
     {
         $builder
             ->add('email')
-            ->add('roles', ChoiceType::class, [
+            ->add('Nom')
+            ->add('Prenom')
+            ->add('plainPassword', PasswordType::class, [
+                'mapped' => false, // Important : on ne mappe pas sur l'entité
+                'required' => true,
+            ]);
+        if ($options['is_admin_form'] ?? false) {
+            $builder->add('roles', ChoiceType::class, [
                 'choices' => [
-                    'Administrateur'=> 'ROLE_ADMIN',
+                    'Administrateur' => 'ROLE_ADMIN',
                     'Formateur' => 'ROLE_FORMATEUR',
                     'Etudiant' => 'ROLE_ETUDIANT',
                 ],
                 'multiple' => true,
                 'expanded' => false,
-            ])
-
-            ->add('plainPassword', PasswordType::class, [
-                'mapped' => false, // Important : on ne mappe pas sur l'entité
-                'required' => true,
-            ])
-            ->add('Nom')
-            ->add('Prenom')
-        ;
+            ]);
+        }
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'is_admin_form' => false, // option par défaut
         ]);
     }
 }
