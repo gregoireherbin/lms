ba3512716a27dc2bb04e3b1a915887d2d3382a56
Grégoire Herbin <melozy@free.fr>
2026-09-17 16:33:32 +0200

    refonte de l'application

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,41 +2,15 @@
 
 namespace App\Controller;
 
-use App\Repository\UserRepository;
-use App\Repository\CoursRepository;
-use App\Repository\EtapeRepository;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index()
+    public function index(): Response
     {
         return $this->render('home.html.twig');
-    }
-
-    #[Route('/utilisateurs', name: 'app_users')]
-    public function users(UserRepository $users)
-    {
-        return $this->render('users.html.twig',[
-            'users'=>$users->findAll()
-        ]);
-    }
-
-    #[Route('/cours', name: 'app_cours')]
-    public function cours(CoursRepository $cours)
-    {
-        return $this->render('cours.html.twig',[
-            'cours'=>$cours->findAll()
-        ]);
-    }
-
-    #[Route('/etapes', name: 'app_etapes')]
-    public function etapes(EtapeRepository $etapes)
-    {
-        return $this->render('etapes.html.twig',[
-            'etapes'=>$etapes->findAll()
-        ]);
     }
 }
